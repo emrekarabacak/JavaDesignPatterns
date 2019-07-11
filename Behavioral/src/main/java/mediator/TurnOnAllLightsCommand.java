1c6a03177a9ded1e46bf0906f985640fd3d709e2
Emre Karabacak <emrekarabacak@outlook.com>
2019-07-11 20:35:18 +0300

    mediator pattern implemented

---
+++ Behavioral/src/main/java/mediator/TurnOnAllLightsCommand.java
@@ -0,0 +1,17 @@
+package mediator;
+
+public class TurnOnAllLightsCommand implements Command {
+
+    private Mediator mediator;
+
+    public TurnOnAllLightsCommand(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void execute() {
+        mediator.turnOnAllLights();
+    }
+
+
+}
